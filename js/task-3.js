ad9f7ac767daaa08b2fa762e437395de6147fe09
MaksFULL <169079873+MaksFULL@users.noreply.github.com>
2024-09-22 13:29:53 +0300

    homework5

--- js/task-3.js
+++ js/task-3.js
@@ -1,29 +1,17 @@
-// Об'єкт профілю користувача на ігровій платформі
-const profile = {
-  username: "Jacob",
-  playTime: 300,
+// Стрілочна функція для сортування користувачів за спаданням кількості друзів
+const sortByDescendingFriendCount = (users) => 
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 
-  // Метод зміни імені профілю
-  changeUsername(newName) {
-    this.username = newName;
-  },
-
-  // Метод для оновлення кількості ігрових годин
-  updatePlayTime(hours) {
-    this.playTime += hours;
-  },
-
-  // Метод для отримання інформації про профіль
-  getInfo() {
-    return `${this.username} has ${this.playTime} active hours!`;
-  }
-};
-
-// Перевірка коректності роботи методів
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+// Перевірка коректності роботи функції
+console.log(
+  sortByDescendingFriendCount([
+    { name: "Moore Hensley", friends: ["Sharron Pace"], gender: "male" },
+    { name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"], gender: "female" },
+    { name: "Ross Vazquez", friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"], gender: "male" },
+    { name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"], gender: "female" },
+    { name: "Carey Barr", friends: ["Jordan Sampson", "Eddie Strong"], gender: "male" },
+    { name: "Blackburn Dotson", friends: ["Jacklyn Lucas", "Linda Chapman"], gender: "male" },
+    { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"], gender: "female" }
+  ])
+);
+// Сортування поверне масив користувачів, відсортованих за кількістю друзів за спаданням
